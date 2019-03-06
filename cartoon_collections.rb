e1c961bbed686eb1f37fec69d1d4de0df4483eda
Charlotte Goldthwaite <chargold201@gmail.com>
2019-03-06 21:07:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,8 @@ def long_planeteer_calls(calls)
   calls.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.any? {|item| snacks.include?(item)}
 end
