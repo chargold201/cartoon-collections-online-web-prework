075535cf5ffbac07e3774543688fe422ca884a9f
Charlotte Goldthwaite <chargold201@gmail.com>
2019-03-06 21:10:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,9 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index { |val,index| puts (index+1).to_s + ". #{val}" }
+  dwarves.each_with_index {|val,index| puts (index+1).to_s + ". #{val}"}
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map { |element| element.capitalize + "!" }
+  planeteer_calls.map {|element| element.capitalize + "!"}
 end
 
 def long_planeteer_calls(calls)
